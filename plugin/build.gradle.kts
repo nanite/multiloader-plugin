7e97cf6be40d487d92cf937d020cea2753240981
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2024-06-21 11:40:20 -0500

    Update NeoGradle

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(21)
 
-version = "0.1.10"
+version = "0.1.11"
 group = "dev.nanite"
 
 repositories {
@@ -44,7 +44,7 @@ dependencies {
 //        exclude(group = "net.minecraftforge", module = "DiffPatch")
 //    }
 //    implementation("org.spongepowered:mixingradle:0.7.38")
-    implementation("net.neoforged.gradle:userdev:7.0.142")
+    implementation("net.neoforged.gradle:userdev:7.0.145")
     // Use JUnit test framework for unit tests
     testImplementation("junit:junit:4.13.1")
     testImplementation ("com.google.code.gson:gson:2.10.1")
